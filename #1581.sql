319532eb133140a629457c3b0737f4bbb0657d70
kumar.krish07022002@gmail.com <kumar.krish07022002@gmail.com>
2025-12-02 20:22:57 +0530

    #1581

---
+++ #1581.sql
@@ -0,0 +1,22 @@
+-- Leetcode Problem 1581:   customer-who-visited-but-did-not-make-any-transactions
+
+-- select v.customer_id, COUNT(*) AS count_no_trans
+-- from Visits v
+-- left join Transactions t
+-- ON v.visit_id = t.visit_id
+-- where t.visit_id is NULL
+-- group by v.customer_id
+
+-- order by count_no_trans DESC;
+
+
+select v.customer_id, COUNT(*) AS count_no_trans
+from Transactions t
+right join Visits v
+ON v.visit_id = t.visit_id
+where t.visit_id is NULL
+group by v.customer_id
+
+order by count_no_trans DESC;
+
+
