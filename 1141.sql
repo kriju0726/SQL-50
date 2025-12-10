3cd132ef384ed4458826317f7b4d65bb1c2a62f1
kumar.krish07022002@gmail.com <kumar.krish07022002@gmail.com>
2025-12-10 10:24:04 +0530

    #1141

---
+++ 1141.sql
@@ -0,0 +1,11 @@
+-- Leetcode Problem 1141:    User Activity for the Past 30 Days I
+-- # Write your MySQL query statement below
+select distinct activity_date as day,
+count(distinct user_id) as active_users
+from Activity
+where activity_date >= '2019-06-28' and activity_date <= '2019-07-27'
+
+group by day;
+
+
+
