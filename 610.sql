83abd45d76590cda28ca180f62e78043533751a2
kumar.krish07022002@gmail.com <kumar.krish07022002@gmail.com>
2025-12-16 23:25:57 +0530

    #610

---
+++ 610.sql
@@ -0,0 +1,15 @@
+-- Leetcode Problem 610: Triangle Judgement
+
+
+SELECT 
+    x,
+    y,
+    z,
+    CASE
+        WHEN x + y > z 
+         AND x + z > y 
+         AND y + z > x
+        THEN 'Yes'
+        ELSE 'No'
+    END AS triangle
+FROM Triangle;
